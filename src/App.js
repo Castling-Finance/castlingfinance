6ae7d3cbdbce2d8cb8227a87ec6937e7cddc073f
Yash Zanwar <yszanwar@gmail.com>
2022-11-14 14:43:16 +0530

    new changes to v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import logo from './logo1.png';
 import './App.css';
+import * as React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          E <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='AppWithin'>
+    <div className="AppUpper">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className='AppBar'>
+      <button className="upper-button" onClick={(e) => {
+      e.preventDefault();
+      window.open('https://castling-finance.gitbook.io/docs/', '_blank', 'noopener,noreferrer');
+      }}>Docs</button>
+      <button className="upper-button" onClick={(e) => {
+      e.preventDefault();
+      window.open('https://twitter.com/Castling_Fin', '_blank', 'noopener,noreferrer');
+      }}>Community</button>
+      </div>
+    </div>
+    <div className="AppLower">
+    <text className='heading'>Castling Finance</text>
+    <text className='subtitle'>Cutting edge financial innovation. Coming soon.</text>
+    <button className="contact-button">Contact Us</button>
+    </div>
+    </div>
     </div>
   );
 }
